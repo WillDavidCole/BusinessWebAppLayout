7474b82ad3a5d2cea11b02dafa1bcebdb3608cce
Will Cole <willcole1987@gmail.com>
2021-11-06 19:56:25 +0000

    adding/testing the new function to add tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,3 +1,13 @@
-addTab = function(e, action){
-    document.getElementsByClassName('tab').innerHTML.append('<button class="tablinks" onclick="openCity(event, \'New\')">London</button>');
+addTab = function() 
+{   
+    var x;
+    for(i in document.getElementById("tab").innerHTML.buttons)
+    {
+        if(i.innerHTML = "+")
+        {
+            document.getElementById("tab").innerHTML += "<button>somename</button>";
+            break;
+        }
+        x+=1;        
+    } 
 }
