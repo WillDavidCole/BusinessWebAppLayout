3522b62ec567f410727829033b0b5b5325b07c90
Will Cole <willcole1987@gmail.com>
2021-11-07 17:17:30 +0000

    Modifications to buttons - editable and closable

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,13 +1,26 @@
 addTab = function() 
-{   
-    var x;
-    for(i in document.getElementById("tab").innerHTML.buttons)
+{
+    var i = $('.tablinks:contains("New")').length + 1;
+    $('<button id="button" type="button" class="tablinks close-button" onclick="reName(event)" onkeydown="completeRename(event)" data-close>Tab' + i + '<span aria-hidden="true">&times;</span></button>').insertBefore('#button-plus');
+};
+
+completeRename = function(e)
+{
+    if(e.keyCode === 13)
     {
-        if(i.innerHTML = "+")
-        {
-            document.getElementById("tab").innerHTML += "<button>somename</button>";
-            break;
-        }
-        x+=1;        
-    } 
-}
+        console.log(e.keyCode);
+        $(this).blur();
+        $(this).attr('contentEditable', false); 
+    }
+};
+
+reName = function(){
+    $(function() {
+        $('.tablinks').on('click', function() {
+            $(this).attr('contentEditable', true); 
+        });
+        $('.tablinks').on('blur', function() {
+            $(this).attr('contentEditable', false); 
+        });
+    })
+};
